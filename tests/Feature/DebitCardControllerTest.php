9a5c2e870fffdafb4b825395ff6eca214131da65
rekidh <89791981+rekidh@users.noreply.github.com>
2023-04-04 17:20:24 +0700

    creat testCustomerCanSeeASingleDebitCardDetails

--- tests/Feature/DebitCardControllerTest.php
+++ tests/Feature/DebitCardControllerTest.php
@@ -101,6 +101,30 @@ class DebitCardControllerTest extends TestCase
     public function testCustomerCanSeeASingleDebitCardDetails()
     {
         // get api/debit-cards/{debitCard}
+        $debitCards = DebitCard::factory()->active()->count(5)->create([
+            'user_id' => $this->user->id,
+        ]);
+
+        Passport::actingAs($this->user);
+
+        // check if user is authenticated
+        $this->assertAuthenticatedAs($this->user);
+
+
+        // check the user can not see debit cards for another user/s
+        $this->getJson('api/debit-cards/' . $debitCards->first()->id)
+            ->assertOk()
+            ->assertJson(
+                fn ($json) => $json->where(
+                    'number',
+                    intval($debitCards->first()->number)
+                )
+                    ->where('type', $debitCards->first()->type)
+                    ->where('is_active', true)
+                    ->where('expiration_date', date_format($debitCards->first()->expiration_date, 'Y-m-d H:i:s'))
+                    ->where('id', $debitCards->first()->id)
+                    ->etc()
+            );
     }
 
     public function testCustomerCannotSeeASingleDebitCardDetails()
